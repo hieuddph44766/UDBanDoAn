29353cc39978eed6705fed04f21ad5a9cd9f70cc
Nguyen Long Vu <nguyenlongvu1001@gmail.com>
2024-11-20 17:31:17 +0700

    create welcome layout

---
+++ app/src/main/java/com/example/duan1_appbandoan/ViewActivity/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.duan1_appbandoan.ViewActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.duan1_appbandoan.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+
+    }
+}
